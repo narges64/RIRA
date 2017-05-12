435a2ce265a63b5901733acdac58caa1261bdcbf
narges <narges.shahidi@gmail.com>
2017-05-12 16:24:39 -0400

    run script

--- run_new.sh
+++ run_new.sh
@@ -1,6 +1,9 @@
 #!/bin/bash 
 
-for dram_cap in 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 
+for dram_cap in 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 32 48 64 72 80 88 96 
 do
-	./ssd page.parameters_SLC result_$dram_cap  dram_capacity=$dram_cap syn_rd_ratio=0.01 > out_${dram_cap} 
+	for rd in 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0 
+	do 	
+		./ssd page.parameters_SLC result_${dram_cap}_${rd}  dram_capacity=$dram_cap syn_rd_ratio=$rd > out_${dram_cap}_${rd} 
+	done 
 done 
